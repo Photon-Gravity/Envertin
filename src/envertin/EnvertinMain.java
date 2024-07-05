f3c9f887790c80b1910230fce1d9183800015da1
Photon_Gravity <photon.gravity@gmail.com>
2024-07-05 19:58:20 +0200

    Clean up main class and add future content load functions.

--- src/envertin/EnvertinMain.java
+++ src/envertin/EnvertinMain.java
@@ -1,40 +1,39 @@
 package envertin;
 
-import arc.*;
-import arc.util.*;
+import arc.util.Log;
 import envertin.content.EnvertinBlocks;
 import envertin.content.EnvertinItems;
 import envertin.content.EnvertinLiquids;
-import mindustry.game.EventType.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.mod.Mod;
 
+@SuppressWarnings("unused")
 public class EnvertinMain extends Mod{
 
     public EnvertinMain(){
-        Log.info("Loaded ExampleJavaMod constructor.");
 
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
     }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading Envertin content...");
+
         EnvertinItems.load();
         EnvertinLiquids.load();
+        //EnvertinStatusEffects.load();
+
+        //EnvertinWeathers.load();
+
+        //EnvertinUnitTypes.load();
 
         EnvertinBlocks.load();
+
+        //EnvertinPlanets.load();
+
+        //EnvertinSectors.load();
+
+        //EnvertinTechTree.load();
+
+        Log.info("Envertin content loaded!");
     }
 
 }
