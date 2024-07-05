486d1deec431710a370cd4d7928d876cc52c3659
Photon_Gravity <photon.gravity@gmail.com>
2024-07-05 16:07:39 +0200

    Added item colors and bundles, also Dryfilm

--- src/envertin/content/EnvertinItems.java
+++ src/envertin/content/EnvertinItems.java
@@ -1,16 +1,31 @@
 package envertin.content;
 
+import envertin.graphics.EnvPal;
 import mindustry.type.Item;
 
 public class EnvertinItems {
-	public static Item antimony, debris, molybdenum, sulfur;
+	public static Item antimony, debris, molybdenum, sulfur, dryfilm, neodymium;
 	public static void load(){
 		antimony = new Item("antimony");
+		antimony.color = EnvPal.antimony;
+		antimony.hardness = 1;
 
 		debris = new Item("debris");
+		debris.color = EnvPal.debris;
 
 		molybdenum = new Item("molybdenum");
+		molybdenum.color = EnvPal.molybdenum;
 
 		sulfur = new Item("sulfur");
+		sulfur.color = EnvPal.sulfur;
+		sulfur.hardness = 2;
+		sulfur.flammability = 0.5f;
+
+		dryfilm = new Item("dryfilm");
+		dryfilm.color = EnvPal.dryfilm;
+
+		neodymium = new Item("neodymium");
+		neodymium.color = EnvPal.neodymium;
+		neodymium.hardness = 3;
 	}
 }
--- src/envertin/graphics/EnvPal.java
+++ src/envertin/graphics/EnvPal.java
@@ -5,5 +5,11 @@ import arc.graphics.Color;
 
 public class EnvPal {
 	public static Color
+	antimony = Color.valueOf("cac5a3"),
+	debris = Color.valueOf("676d72"),
+	molybdenum = Color.valueOf("738a86"),
+	sulfur = Color.valueOf("807754"),
+	dryfilm = Color.valueOf("ce97b9"),
+	neodymium = Color.valueOf("524d4f"),
 	outline = Color.valueOf("121413");
 }
